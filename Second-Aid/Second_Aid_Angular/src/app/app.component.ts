fc9f0d45944babc476fa74ee65e571bb2c45b4ce
Dyhoereos <dennis.chau@outlook.com>
2016-11-29 13:27:30 -0800

    bugfix

--- Second-Aid/Second_Aid_Angular/src/app/app.component.ts
+++ Second-Aid/Second_Aid_Angular/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AuthService]
 })
+
 export class AppComponent {
   title = 'app works!';
   
